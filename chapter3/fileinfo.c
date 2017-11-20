a820f45e21e6323fc222aa105d389ad1d1b184e8
book <book@book-desktop.(none)>
2017-11-20 14:45:49 +0800

    将用户/组ID转换成字符串

--- chapter3/fileinfo.c
+++ chapter3/fileinfo.c
@@ -1,6 +1,8 @@
 #include "stdio.h"
 #include "sys/types.h"
 #include "sys/stat.h"
+#include "pwd.h"
+#include "grp.h"
 
 void showtime(long timeval)
 {
@@ -33,12 +35,38 @@ void showmode(long stmode)
 	printf("%s ", str);
 }
 
+char *uid_to_name(uid_t uid)
+{
+	static char numstr[10];
+	struct passwd *getpwuid(), *pw_ptr;
+
+	if((pw_ptr = getpwuid(uid)) == NULL){
+		sprintf(numstr, "%d", uid);
+		return numstr;
+	}
+	else
+		return pw_ptr->pw_name;
+}
+
+char *gid_to_name(gid_t gid)
+{
+	static char numstr[10];
+	struct group *getgrgid(), *grp_ptr;
+
+	if((grp_ptr = getgrgid(gid)) == NULL){
+		sprintf(numstr, "%d", gid);
+		return numstr;
+	}
+	else
+		return grp_ptr->gr_name;
+}
+
 void show_stat_info(char *fname, struct stat *buf)
 {
 	showmode(buf->st_mode);
 	printf("%d ", buf->st_nlink);
-	printf("%o ", buf->st_uid);
-	printf("%d ", buf->st_gid);
+	printf("%s ", uid_to_name(buf->st_uid));   //将st_uid传递给getpwuid(),返回一个passwd结构体指针
+	printf("%s ", gid_to_name(buf->st_gid));   //将st_gid传递给getgrgid(),返回一个group结构体指针
 	printf("%d ", buf->st_size);
 	showtime(buf->st_mtime);
 	printf("%s\n", fname);
