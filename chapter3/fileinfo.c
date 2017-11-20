e87c527e22a1ee93c3a6c65414d0a129a2060482
book <book@book-desktop.(none)>
2017-11-20 14:07:22 +0800

    显示了类型/用户/组/其他 的具体信息

--- chapter3/fileinfo.c
+++ chapter3/fileinfo.c
@@ -7,20 +7,41 @@ void showtime(long timeval)
 	char *cp;
 	cp = ctime(&timeval);
 	
-	printf("	%12.12s\n", cp + 4);
+	printf("%12.12s ", cp + 4);
+}
+
+void showmode(long stmode)
+{
+	char str[10];
+	strcpy(str, "----------");
+	if(S_ISDIR(stmode)) str[0] = 'd';
+	if(S_ISCHR(stmode)) str[0] = 'c';
+	if(S_ISBLK(stmode)) str[0] = 'b';
+
+	if(stmode & S_IRUSR) str[1] = 'r';
+	if(stmode & S_IWUSR) str[2] = 'w';
+	if(stmode & S_IXUSR) str[3] = 'x';
+
+	if(stmode & S_IRGRP) str[4] = 'r';
+	if(stmode & S_IWGRP) str[5] = 'w';
+	if(stmode & S_IXGRP) str[6] = 'x';
+
+	if(stmode & S_IROTH) str[7] = 'r';
+	if(stmode & S_IWOTH) str[8] = 'w';
+	if(stmode & S_IXOTH) str[9] = 'x';
+
+	printf("%s ", str);
 }
 
 void show_stat_info(char *fname, struct stat *buf)
 {
-	
-	printf("	mode    :  %o\n", buf->st_mode);
-	printf("	links   :  %d\n", buf->st_nlink);
-	printf("	user    :  %o\n", buf->st_uid);
-	printf("	group   :  %d\n", buf->st_gid);
-	printf("	size    :  %d\n", buf->st_size);
+	showmode(buf->st_mode);
+	printf("%d ", buf->st_nlink);
+	printf("%o ", buf->st_uid);
+	printf("%d ", buf->st_gid);
+	printf("%d ", buf->st_size);
 	showtime(buf->st_mtime);
-	printf("	modtime :  %d\n", buf->st_mtime);
-	printf("	name    :  %s\n", fname);
+	printf("%s\n", fname);
 
 }
 
