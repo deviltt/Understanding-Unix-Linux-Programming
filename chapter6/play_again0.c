9cab2dbd962021d6efa450de2ef1e3775c99a905
book <book@book-desktop.(none)>
2017-11-21 17:18:48 +0800

    使用非阻塞模式实现超时响应

--- chapter6/play_again0.c
+++ chapter6/play_again0.c
@@ -1,36 +1,52 @@
 #include "stdio.h"
 #include "termios.h"
-#define QUESTION "Do you want another transaction"
+#include "fcntl.h"
+#include "string.h"
 
-int get_response(char *question)
+#define QUESTION "Do you want another transaction"
+#define sleeptime 2
+#define tries 3
+#define BEEP putchar('\a')
+
+int get_ok_char()
+{
+	int c;
+	while((c = getchar()) != EOF && strchr("yYnN",c) == NULL);
+	return c;
+}
+
+int get_response(char *question, int maxtries)
 {
 	int input;
 	printf("%s (y/n)?", question);
+	fflush(stdout);
 	while(1){
-		switch(input = getchar()){
-			case 'y':
-			case 'Y': return 0;
-			case 'n':
-			case 'N':
-			case EOF: return 1;
-/*			default : 
-				  printf("\ncan't understand %c, ", input);
-				  printf("please type y or n \n");
-*/
+		sleep(sleeptime);
+		input = tolower(get_ok_char());
+		if(input == 'y')
+			return 0;
+		if(input == 'n')
+			return 1;
+		if(maxtries-- == 0)
+			return 2;
+		BEEP;
 		}
-	}
 }
 
-/*
 int tty_mode(int how)
 {
 	static struct termios original_mode;
-	if(how == 0)
+	static int original_flags;
+	if(how == 0){
 		tcgetattr(0, &original_mode);
-	else
-		return tcsetattr(0, TCSANOW, &original_mode);
+		original_flags = fcntl(0, F_GETFL);
+	}
+	else{
+		tcsetattr(0, TCSANOW, &original_mode);
+		fcntl(0, F_SETFL, original_flags);
+	}
 }
-*/
+
 void set_crmode()
 {
 	struct termios ttystate;
@@ -41,12 +57,22 @@ void set_crmode()
 	tcsetattr(0, TCSANOW, &ttystate);
 }
 
+void set_nodelay_mode()
+{
+	int termflags;
+	termflags = fcntl(0, F_GETFL);
+	termflags |= O_NDELAY;
+	fcntl(0, F_SETFL, termflags);
+}
+
 int main()
 {
 	int response;
-	//tty_mode(0);
+	tty_mode(0);
 	set_crmode();
-	response = get_response(QUESTION);
+	set_nodelay_mode();
+	response = get_response(QUESTION, tries);
+	tty_mode(1);
 	printf("\n");
 	return response;
 }
