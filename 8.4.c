18565523b4dce98a9dd17383da7c70b431795c78
book <book@book-desktop.(none)>
2017-11-27 01:10:15 +0800

    测试

---
+++ 8.4.c
@@ -0,0 +1,22 @@
+#include "stdio.h"
+#include "string.h"
+
+int main()
+{
+	int fd;
+	int pid;
+	char msg1[] = "Test 1 2 3..\n";
+	char msg2[] = "Hello, hello\n";
+
+	if((fd = creat("testfile", 0644)) == -1)
+		return 0;
+	if(write(fd, msg1, strlen(msg1)) == -1)
+		return 0;
+
+	if((pid = fork()) == -1)
+		return 0;
+	if(write(fd, msg2, strlen(msg2)) == -1)
+		return 0;
+	close(fd);
+	return 0;
+}
