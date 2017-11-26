07643c449889f8c9fd8577ff31b813afc2156d60
book <book@book-desktop.(none)>
2017-11-26 23:21:47 +0800

    实现一个shell，并且可以重复输入命令

---
+++ psh2.c
@@ -0,0 +1,61 @@
+#include "stdio.h"
+#include "signal.h"
+#include "string.h"
+#include "stdlib.h"
+
+#define MAXARGS 20
+#define ARGLEN  100
+
+char * makestring(char *buf)
+{
+	char *cp;
+	buf[strlen(buf) - 1] = '\0';
+	cp = (char *)malloc(strlen(buf) + 1);
+	if(cp == NULL){
+		fprintf(stderr, "no memory\n");
+		exit(1);
+	}
+	strcpy(cp, buf);
+	return cp;
+}
+
+void execute(char *arglist[])
+{
+	int pid,exitstatus;
+	
+	pid = fork();
+	switch(pid){
+		case -1:
+			perror("fork failed");	
+			exit(1);
+		case  0:
+			execvp(arglist[0], arglist);
+			perror("execvp failed");
+			exit(1);	
+		default:
+			while(wait(&exitstatus) != pid);
+			printf("child exited with status %d, %d\n", exitstatus >> 8, exitstatus & 0377);
+	}
+}
+
+int main()
+{
+	char *arglist[MAXARGS];
+	char argbuf[ARGLEN];
+	int numargs;
+
+	numargs = 0;
+	while(numargs < MAXARGS){
+		printf("Arg[%d]?", numargs);
+		if(fgets(argbuf, ARGLEN, stdin) && *argbuf != '\n')
+			arglist[numargs++] = makestring(argbuf); //在每个字符串后面添加空字符即NULL
+		else{
+			if(numargs > 0){
+				arglist[numargs] = NULL;
+				execute(arglist);
+				numargs = 0;
+			}
+		}
+	}
+	return 0;
+}
