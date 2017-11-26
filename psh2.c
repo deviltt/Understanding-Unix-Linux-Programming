1593436f77a0475cd5d7ab3e263cdada35dfbcf2
book <book@book-desktop.(none)>
2017-11-27 00:51:28 +0800

    添加了一点注释

--- psh2.c
+++ psh2.c
@@ -9,7 +9,7 @@
 char * makestring(char *buf)
 {
 	char *cp;
-	buf[strlen(buf) - 1] = '\0';
+	buf[strlen(buf) - 1] = '\0';	//strlen计算不包括含终止NULL字节的字符串长度，sizeof包括
 	cp = (char *)malloc(strlen(buf) + 1);
 	if(cp == NULL){
 		fprintf(stderr, "no memory\n");
@@ -29,11 +29,11 @@ void execute(char *arglist[])
 			perror("fork failed");	
 			exit(1);
 		case  0:
-			execvp(arglist[0], arglist);
+			execvp(arglist[0], arglist);	//通过调用exec函数族在当前进程中执行一个新的程序
 			perror("execvp failed");
 			exit(1);	
 		default:
-			while(wait(&exitstatus) != pid);
+			while(wait(&exitstatus) != pid);	//当接受到子进程的退出信息时就会继续执行被挂起的进程
 			printf("child exited with status %d, %d\n", exitstatus >> 8, exitstatus & 0377);
 	}
 }
