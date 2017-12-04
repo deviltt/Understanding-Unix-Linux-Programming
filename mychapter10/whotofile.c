2a3ee1d58837213f876f414d96b6a36a38110226
book <book@computer1.(none)>
2017-12-04 20:18:48 +0800

    之前调试不成功是因为O_TRUNC是将文件长度截断为0

--- mychapter10/whotofile.c
+++ mychapter10/whotofile.c
@@ -8,16 +8,21 @@
 
 int main()
 {
-	int pid, fd;
+	int pid;
 
 	if((pid = fork()) == EOF)
 		oops("fork error", 1);
 
 	if(pid == 0){
 		close(1);
-		fd = creat("userlist", 0644);	//最小分配原则
-		execlp("who", "who", NULL);
-		perror("execlp");
+	//	fd = creat("userlist", 0644);	//最小分配原则
+		if(open("userlist", O_WRONLY | O_APPEND | O_CREAT, 0644) != 1){	
+			oops("open error", 1);
+		}
+		else{
+			execlp("who", "who", NULL);
+			perror("execlp");
+		}
 		exit(1);
 	}
 	else{
