34404077416092e123e9b55b97ad81d40c62a17b
book <book@computer1.(none)>
2017-12-04 14:12:37 +0800

    重定向标准输出到另一个文件

---
+++ mychapter10/whotofile.c
@@ -0,0 +1,28 @@
+/* 每个进程默认使用文件描述符0，1，2作为标准输入，标准输出，标准出错 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+#define oops(s, x) {perror(s); exit(x);}
+
+int main()
+{
+	int pid, fd;
+
+	if((pid = fork()) == EOF)
+		oops("fork error", 1);
+
+	if(pid == 0){
+		close(1);
+		fd = creat("userlist", 0644);	//最小分配原则
+		execlp("who", "who", NULL);
+		perror("execlp");
+		exit(1);
+	}
+	else{
+		wait(NULL);
+		printf("done, results are in userlist\n");
+	}
+	return 0;
+}
