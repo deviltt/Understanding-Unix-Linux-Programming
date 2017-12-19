976237f5ef36a70e288a342ed8af19a01afaca07
deviltt <tt416926063@qq.com>
2017-12-19 09:02:18 +0800

    pthread_join(tid1, &tret); pthread_exit(void *rval_ptr);

--- printnum.c
+++ printnum.c
@@ -10,13 +10,15 @@ void *print_cnt(void *m)
 		printf("counter = %d\n", counter);
 		sleep(1);
 	}
-	return NULL;
+//	return ((void *)1);
+	pthread_exit((void *)1);
 }
 
 int main()
 {
 	pthread_t tid1;
 	int err, i;
+	void *tret;
 
 	if((err = pthread_create(&tid1, NULL, print_cnt, NULL)) != 0)
 		perror("can't creat thread 1");
@@ -25,6 +27,7 @@ int main()
 		counter++;
 		sleep(1);
 	}
-	if((err = pthread_join(tid1, NULL)) != 0)
+	if((err = pthread_join(tid1, &tret)) != 0)
 		perror("can't join thread 1");
+	printf("thread 1 exit code %ld\n", (long)tret);
 }
